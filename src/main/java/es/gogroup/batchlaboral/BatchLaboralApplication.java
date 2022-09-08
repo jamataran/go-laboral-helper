4ce17eaa60e27e3bf25a45a878f229989f5a0f78
luisroperodoval <luisrym@gmail.com>
2022-09-08 08:39:33 +0200

    Issue 1: Working on entity class

--- src/main/java/es/gogroup/batchlaboral/BatchLaboralApplication.java
+++ src/main/java/es/gogroup/batchlaboral/BatchLaboralApplication.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
